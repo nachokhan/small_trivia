8ad132c1812234764d577a34d5d6356492ae5d5d
Nachokhan <ignacio.rigoni@gmail.com>
2020-05-12 15:07:44 -0300

    Now shows the results

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,6 @@ import 'package:first_app/Model/Question.dart';
 import 'package:first_app/wquiz.dart';
 import 'package:flutter/material.dart';
 
-// void main() {
-//   var obj = MyApp();
-//   runApp(obj);
-// }
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -19,26 +14,37 @@ class MyApp extends StatefulWidget {
 class MyAppState extends State<MyApp> {
   var qIndex = 0;
   var questionnaire = Questionnaire();
+  var userAnswers = List<int>();
 
   MyAppState() {
     questionnaire.testFill();
   }
 
   void buttonPressed(int rpta) {
-    if (qIndex >= questionnaire.getAllQuestions().length-1) {
-      print("No more QUESTIONS! :(");
+    if (userAnswers.length <= qIndex) {
+      userAnswers.add(rpta);
+    }
+    else {
+      userAnswers[qIndex] = rpta;
+    }
+    
+    goQuestionForward();    
+    print("Se tocó la Rpta: $rpta");
+  }
+
+  void goQuestionForward() {
+    if (qIndex >= questionnaire.getAllQuestions().length) {
+      print("No more than 'lenght' please!");
       return;
     }
     setState(() {
       qIndex++;
     });
-
-    print("Se tocó la Rpta: $rpta");
   }
 
-  void backButtonPressed() {
-    if (qIndex <= 0) {
-      print("You can't go back for ever, man! Grow up!");
+  void goQuestionBackward() {
+    if (qIndex < 0) {
+      print("Not less than zero, please!!");
       return;
     }
     setState(() {
@@ -48,13 +54,12 @@ class MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-
     var scaf = Scaffold(
       appBar: AppBar(
         title: Text("Preguntas boludas!"),
         backgroundColor: Color.fromARGB(255, 255, 100, 0),
       ),
-      body: WQuiz(questionnaire.getQuestion(qIndex), buttonPressed, backButtonPressed),
+      body: showBody(),
     );
 
     return MaterialApp(
@@ -62,5 +67,34 @@ class MyAppState extends State<MyApp> {
     );
   }
 
-  
+  Widget showBody() {
+    Widget result;
+
+    if (qIndex >= questionnaire.getLength()) {
+
+      var results = questionnaire.checkAllAnswers(this.userAnswers);
+      int accerts = 0;
+      for (var x in results) if (x == true) accerts++;
+      var total = questionnaire.getLength();
+      var text = "Accerted $accerts of $total";
+
+      result = Center(
+        child: Text(
+          "YOU FINISHED!! \n" + text,
+          style: TextStyle(fontSize: 35),
+        ),
+      );
+    } else if (qIndex < 0) {
+      result = Center(
+        child: Text(
+          "YOU CAN'T GO BACK FOR EVER! GROW UP, MAN!",
+          style: TextStyle(fontSize: 25),
+        ),
+      );
+    } else {
+      result = WQuiz(
+          questionnaire.getQuestion(qIndex), buttonPressed, goQuestionBackward);
+    }
+    return result;
+  }
 }
